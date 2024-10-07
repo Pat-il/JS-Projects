2b403ec6bd7e6e44bd1b008e817ef69734d661c2
Tejas <90674200+Pat-il@users.noreply.github.com>
2024-10-07 09:11:18 +0530

    Basics Of JS

---
+++ Day1/first.js
@@ -0,0 +1,37 @@
+console.log("Welcome to apna College");
+age = 24;
+price = 23.34;
+fullname = "Tony Stark";
+console.log(age);
+console.log(price);
+console.log(fullname);
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 28,
+    cgpa : 8.2,
+    isPass : true
+}
+
+console.log(student["fullName"]);
+console.log(student.age);
+
+student["fullName"] = "Rahul Sharma";
+console.log(student);
+
+
+// Que 1 : Create Product Object
+const product = {
+    title : "Ball Pen", 
+    rating : 4,
+    offer : 5,
+    price : 269
+}
+
+// Que 2 : Create a const object called profile
+const profile = {
+    name : "Sharadha Khapra",
+    follower : 132,
+    isFollow : true,
+    follow : 23
+}
