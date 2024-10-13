842b1b4dc4e3b5b0630e01440f87adee0a8179af
Tejas <90674200+Pat-il@users.noreply.github.com>
2024-10-13 09:38:10 +0530

    DOM

---
+++ Day6/script.js
@@ -0,0 +1,36 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+// let button = document.getElementById("myId");
+// console.log(button);
+
+// let headings = document.getElementsByClassName("heading");
+// console.dir(headings);
+
+// let paras = document.getElementsByTagName("p");
+// console.log(paras);
+
+// let firstelem = document.querySelector("p");
+// console.dir(firstelem);
+
+// let allElem = document.querySelectorAll("p");
+// console.log(allElem);
+
+// let firClass = document.querySelector(".heading");
+// console.dir(firClass);
+
+// let allClass = document.querySelectorAll(".heading");
+// console.log(allClass);
+
+// let butId = document.querySelector("#myId");
+// console.dir(butId);
+
+// console.dir(document.body.firstChild);
+let div = document.querySelector("div");
+console.log(div);
+
+let li = document.querySelector("li");
+console.log(li);
+
+let lia = document.querySelectorAll("li");
+console.log(lia);
